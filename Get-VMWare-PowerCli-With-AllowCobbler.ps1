29ac8d9d895a4232aff1264a56c1913c4346e2dc
JBBR <jbk.broguiere@gmail.com>
2025-07-07 10:15:28 +0200

    Added script to send command to a VM via the console

---
+++ Get-VMWare-PowerCli-With-AllowCobbler.ps1
@@ -0,0 +1,20 @@
+﻿<# 
+When installing PowerCli, you may encouter this error: 
+
+PackageManagement\Install-Package : The following commands are already available on this
+system:'Export-VM,Get-VM,Get-VMHost,Move-VM,New-VM,Remove-VM,Restart-VM,Set-VM,Set-VMHost,Start-VM,Stop-VM,Suspend-VM'. This module
+
+This is because the Hyper-V module is already installed and both VMWare and Hyper-V PowerShell modules contain functions with the same name: Export-VM,Get-VM,Get-VMHost,Move-VM,New-VM,Remove-VM,Restart-VM,Set-VM,Set-VMHost,Start-VM,Stop-VM,Suspend-VM
+
+To solve this issue, you can use the "AllowClobber" and import the VMWare module even if there are conflicting commands. The last module imported will be executed, the. 
+
+I means, after installing the VMWare Module, "Get-VM" will be for VMWare. To use the hyper-V variant, you will have to precise the module name: "Get-Command Hyper-V\Get-VM"
+
+https://pkisharp.github.io/ACMESharp-docs/Why-AllowClobber.html
+https://orelfichman.com/blog/dealing-with-conflicting-powershell-cmdlets
+#>
+Install-Module -Name VCF.PowerCLI -AllowClobber
+
+
+Get-Command Hyper-V\Get-VM
+Get-Command VMware.VimAutomation.Core\Get-VM
---
+++ Send-Comman-To-Vm.ps1
@@ -0,0 +1,10 @@
+﻿$vcenterCred = Get-Credential -Message "Enter vCenter credentials"
+$vmCred = Get-Credential -Message "Enter target VM credentials"
+$VIServer = Read-Host "Enter target vCenter"
+$VMName = Read-Host "Enter target VM name"
+$command = Read-Host "Enter the command to executed in the VM" 
+
+VMware.VimAutomation.Core\Connect-VIServer -Server $VIServer -Credential $vcenterCred -ErrorAction Stop -Force
+$vm = VMware.VimAutomation.Core\Get-VM -Name $VMName
+$result = VMware.VimAutomation.Core\Invoke-VMScript -VM $vm -ScriptText $command -GuestCredential $vmCred -ErrorAction Stop
+$result.ScriptOutput
