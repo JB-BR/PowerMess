e9801858aa9591ac5b7eeb8899b2557781f3645b
JBBR <jbk.broguiere@gmail.com>
2025-07-25 16:27:21 +0200

    new command

---
+++ Get-System-IPs.ps1
@@ -0,0 +1,2 @@
+﻿# PowerShell Equivalent of the Linux 'ip addr | grep -E "\d{1,3}(\.\d{1,3}){3}"' command
+ipconfig | Select-String -Pattern '\d{1,3}(\.\d{1,3}){3}' -AllMatches | ForEach-Object { $_.Matches.Value }
