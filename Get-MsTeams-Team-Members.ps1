2286961f853d5bfcd1f9df91f76797d88c57c520
JBBR <jbk.broguiere@gmail.com>
2025-08-19 12:07:32 +0200

    Get the members of a team in microsoft teams

---
+++ Get-MsTeams-Team-Members.ps1
@@ -0,0 +1,15 @@
+﻿# Install-Module -Name MicrosoftTeams
+Connect-MicrosoftTeams
+
+$teamName = Read-Host "Enter the display name of the Team"
+
+# We use to display name to retrive the team. Other filters are possible, kike the ID or the Mail nickname
+# https://learn.microsoft.com/en-us/powershell/module/microsoftteams/get-team?view=teams-ps
+
+$team = Get-Team -DisplayName $teamName
+
+# We now use the team ID to terve the users member of this team
+$users = Get-TeamUser -GroupID $team.GroupId
+
+# Display the reulst
+$users
---
+++ Get-VMs.ps1
@@ -0,0 +1,4 @@
+﻿$vcenterCred = Get-Credential -Message "Enter vCenter credentials"
+$VIServer = Read-Host "Enter target vCenter"
+VMware.VimAutomation.Core\Connect-VIServer -Server $VIServer -Credential $vcenterCred -ErrorAction Stop -Force
+$virtualMachines = Get-VM
