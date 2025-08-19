2de428e69e94c2b42f2b15041e5a111996996e08
JBBR <jbk.broguiere@gmail.com>
2025-08-19 13:31:09 +0200

    get user infos from Azure AD

---
+++ Get-MsGraph-User.ps1
@@ -0,0 +1,10 @@
+﻿# Install-Module Microsoft.Graph
+Connect-MgGraph
+
+$user_id = Read-Host "Enter the users e-mail address"
+
+$user = Get-MgUser -UserId $user_id
+
+$user 
+
+$user | Format-List *
